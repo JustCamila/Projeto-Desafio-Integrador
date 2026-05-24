b4cfead3987006221372b864f61fb9ced6a31c5c
Camila Vujanski <camilavujanskil29@gmail.com>
2026-05-24 20:08:31 -0300

    Conexão com o banco

---
+++ scr/Conexao.java
@@ -0,0 +1,14 @@
+package scr;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+private static final String URL = "jdbc:mysql://localhost:3306/loja_toptop";
+private static final String USER = "root";
+private static final String PASS = ""; 
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
