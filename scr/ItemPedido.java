88929421a8705460e521b9897e15ed5c95ae98cb
joaomarcelo0202 <joaomarcelodossantosoliveirajm@gmail.com>
2026-05-31 22:20:42 -0300

    criei modelo ItemPedido com validacoes de quantidade

--- scr/ItemPedido.java
+++ scr/ItemPedido.java
@@ -1,5 +1,10 @@
 package scr;
 
 public class ItemPedido {
-    
+    private final int id; 
+    private final Produto produto;
+    private final int quantidade;
+    private final double precoUnitario;
+    private final double precoTotal;
+
 }
