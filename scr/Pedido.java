43d5cd9c3e217f9c4f07b95b1707bcf0d67fdc82
joaomarcelo0202 <joaomarcelodossantosoliveirajm@gmail.com>
2026-05-31 22:23:24 -0300

    criei modelo Pedido com suporte a múltiplos itens e status

--- scr/Pedido.java
+++ scr/Pedido.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 package scr;
 
 public class Pedido {
-    
+    private final int id;
+    private final Cliente cliente;
+    private final List<ItemPedido> itens;
+    private StatusPedido status;
+
 }
