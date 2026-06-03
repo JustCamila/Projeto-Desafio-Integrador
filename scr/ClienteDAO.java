c61b4027c27704b78ca956ee521fbaa8663fee63
Rhauan <engs-rhauansilva@camporeal.edu.br>
2026-06-03 15:30:47 -0300

    selects do banco dentro do java

--- scr/ClienteDAO.java
+++ scr/ClienteDAO.java
@@ -1,7 +1,11 @@
 package scr;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ClienteDAO {
 
@@ -21,4 +25,29 @@ public class ClienteDAO {
             System.err.println("Erro ao salvar: " + e.getMessage());
         }
     }
+
+public List<Cliente> listarTodos() {
+    List<Cliente> clientes = new ArrayList<>();
+    String sql = "SELECT nome, email FROM clientes"; // <-- puxa os nomes e emails do banco 
+
+    try (Connection conn = Conexao.conectar();  // o try está aqui pra garantir que seja fechado corretamente mesmo ouvendo um erro na execução
+         PreparedStatement stmt = conn.prepareStatement(sql);
+         ResultSet rs = stmt.executeQuery()) {
+
+        while (rs.next()) {
+            String nome = rs.getString("nome");
+            String email = rs.getString("email");
+
+            Cliente cliente = new Cliente(nome, email);
+            clientes.add(cliente);
+        }
+
+    } catch (SQLException e) {
+        System.err.println("Erro ao listar clientes: " + e.getMessage());
+    }
+
+    return clientes;
+}
+
+
 }
