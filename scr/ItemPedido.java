111386be36f8cbeb4e2ac99b6d5b82ac4c5e803c
joaomarcelo0202 <joaomarcelodossantosoliveirajm@gmail.com>
2026-06-03 19:26:31 -0300

    criando cosntrutores para quando criar um novo item e para ler o item no banco de dados

--- scr/ItemPedido.java
+++ scr/ItemPedido.java
@@ -7,4 +7,29 @@ public class ItemPedido {
     private final double precoUnitario;
     private final double precoTotal;
 
+    //construtorzinho para quando criarmos um novo item
+    public ItemPedido(Produto produto, int quantidade) {
+        if (produto == null) {
+            throw new IllegalArgumentException("É obrigatório o produto no item do pedido.");
+        }
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("A quantidade do item deve ser maior que zero.");
+        }
+        
+        this.id = 0; 
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = produto.getPreco();
+        this.precoTotal = this.precoUnitario * this.quantidade;
+    }
+
+    //construtorzinho para quando formos ler um item do banquin de dados
+    public ItemPedido(int id, Produto produto, int quantidade, double precoUnitario) {
+        this.id = id;
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+        this.precoTotal = this.precoUnitario * this.quantidade;
+    }
+
 }
