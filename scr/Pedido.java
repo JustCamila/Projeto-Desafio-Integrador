97ec49c0ccf24e5e088ef2d5675ccbe5e39086c5
joaomarcelo0202 <joaomarcelodossantosoliveirajm@gmail.com>
2026-06-04 21:46:16 -0300

    criado construtor para criar um novo pedido e um construtor para quando o banco instanciar um pedido já existente

--- scr/Pedido.java
+++ scr/Pedido.java
@@ -10,4 +10,25 @@ public class Pedido {
     private final List<ItemPedido> itens;
     private StatusPedido status;
 
+// contrutorzin para criar um novo pedido
+
+public Pedido(Cliente cliente) {
+        if (cliente == null) {
+            throw new IllegalArgumentException("O pedido precisa ter um cliente associado.");
+        }
+        this.id = 0;
+        this.cliente = cliente;
+        this.status = StatusPedido.ABERTO;
+        this.itens = new ArrayList<>();
+    }
+
+//construtor para quando o banco instanciar um pedido já existente
+    public Pedido(int id, Cliente cliente, StatusPedido status, List<ItemPedido> itens) {
+        this.id = id;
+        this.cliente = cliente;
+        this.status = status;
+        this.itens = itens != null ? itens : new ArrayList<>();
+    }
 }
+
+
