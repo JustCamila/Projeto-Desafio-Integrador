a095ca2f5ab94ec4218efc473a7dde2744f00b9f
Camila <camilavujanskil29@gmail.com>
2026-06-05 17:51:52 -0300

    README OK

---
+++ Banco de dados/script.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE loja_toptop
+character set utf8mb4
+collate utf8mb4_general_ci;
+
+use loja_toptop;
+
+create table clientes(
+	id_cliente int primary key auto_increment,
+    nome varchar(150) not null,
+    email varchar(150) unique
+);
+
+create table produtos(
+	id_produto int primary key auto_increment,
+    nome_produto varchar(100) not null,
+    estoque int not null,
+    preco double,
+	categoria varchar(50)
+);
+
+create table pedido(
+	id_pedido int primary key auto_increment,
+    cliente_id int,
+    status varchar(50) not null,
+    foreign key(cliente_id) references clientes(id_cliente)
+);
+
+create table itens_pedidos(
+	produto_id int,
+    pedido_id int,
+	quantidade_itens int not null,
+    foreign key(pedido_id) references pedido(id_pedido),
+    foreign key(produto_id) references produtos(id_produto)
+);
+
